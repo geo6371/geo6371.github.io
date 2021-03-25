eb3cab05bddce74e9d243dc8c014e46c6525f0ae
geo6371 <jens-weise@hotmail.de>
2021-03-25 17:03:01 +0100

    pupup verbessert

--- nz/main.js
+++ nz/main.js
@@ -17,6 +17,9 @@ const map = L.map("map", {
 });
 
 let mrk = L.marker([stop.lat, stop.lng]).addTo(map);
-mrk.bindPopup("Wellington").openPopup();
+mrk.bindPopup(`
+    <h4>Stop ${stop.nr}: ${stop.name}</h4>
+    <p><i class="fas fa-external-link-alt" mr-3></i> <a href="${stop.wikipedia}">Read about stop in Wikipedia</a></p>
+`).openPopup();
 
 console.log(document.querySelector("#map"))
