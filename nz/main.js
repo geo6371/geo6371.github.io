a9fbafcb6ec3cd8d8e1c47cf2c07f0617570a40b
geo6371 <jens-weise@hotmail.de>
2021-03-25 17:57:56 +0100

    Pulldown Vorschleife befuellen

--- nz/main.js
+++ nz/main.js
@@ -16,12 +16,17 @@ const map = L.map("map", {
     ]
 });
 
-
+let nav = document.querySelector("#navigation");
+console.log(nav);
 
 console.log(ROUTE);
 for (let entry of ROUTE) {
     console.log(entry);
     
+nav.innerHTML += `
+    <option value="${entry.user}">Stop ${entry.nr}: ${entry.name}</option>
+`;
+
     let mrk = L.marker([entry.lat, entry.lng]).addTo(map);
     mrk.bindPopup(`
         <h4>Stop ${entry.nr}: ${entry.name}</h4>
