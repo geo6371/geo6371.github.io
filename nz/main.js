8b050b7453565a09fc5fef8ffa9250ed99492d83
geo6371 <jens-weise@hotmail.de>
2021-03-25 16:15:18 +0100

    map

--- nz/main.js
+++ nz/main.js
@@ -1,3 +1,9 @@
-const map = L.map("map");
-//-41.2875°, 174.776111°
+const map = L.map("map", {
+    center: [ -41.2875, 174.776111 ], 
+    zoom: 13
+    layers: [
+        L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png")
+    ]
+});
+//-41.2875, 174.776111
 console.log(document.querySelector("#map"))
