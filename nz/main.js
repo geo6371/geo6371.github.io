f70dd2787901d6b83cae721bc6bfc6b3e83de295
geo6371 <jens-weise@hotmail.de>
2021-03-25 16:22:55 +0100

    Popup hinzugefuegt

--- nz/main.js
+++ nz/main.js
@@ -1,9 +1,12 @@
 const map = L.map("map", {
     center: [ -41.2875, 174.776111 ], 
-    zoom: 13
+    zoom: 13,
     layers: [
         L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png")
     ]
 });
-//-41.2875, 174.776111
+
+let mrk = L.marker([ -41.2875, 174.776111 ]) .addTo(map);
+mrk.bindPopup("Wellington") .openPopup();
+
 console.log(document.querySelector("#map"))
