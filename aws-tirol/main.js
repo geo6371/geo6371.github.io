db8542bf653309a628a79e3aff1160d6105a040b
geo6371 <jens-weise@hotmail.de>
2021-04-15 16:42:39 +0200

    wetterstation Marker gesetzt

--- aws-tirol/main.js
+++ aws-tirol/main.js
@@ -12,7 +12,28 @@ let map = L.map("map", {
 let layerControl = L.control.layers({
     "BasemapAT.grau": basemapGray,
     "BasemapAT.orthofoto": L.tileLayer.provider('BasemapAT.orthofoto'),
+    "BasemapAT.overlay+ortho": L.layerGroup([
+        L.tileLayer.provider('BasemapAT.orthofoto'),
+        L.tileLayer.provider('BasemapAT.overlay')
+    ]),
     "BasemapAT.terrain": L.tileLayer.provider('BasemapAT.terrain'),
     "BasemapAT.surface": L.tileLayer.provider('BasemapAT.surface'),
     "OpenTopoMap": L.tileLayer.provider('OpenTopoMap')
 }) .addTo(map);
+
+let awsUrl = 'https://wiski.tirol.gv.at/lawine/produkte/ogd.geojson';
+
+fetch(awsUrl)
+    .then(response => response.json())
+    .then(json => {
+        console.log('Daten konvertieren: ', json);
+        for (station of json.features) {
+            console.log('Station: ', station);
+            let marker = L.marker(
+                [
+                    station.geometry.coordinates[1], station.geometry.coordinates[0]
+                ]);
+            marker.addTo(map);
+        }
+});
+    
