4b1806e1158558473947baeefda91e148306558e
geo6371 <jens-weise@hotmail.de>
2021-04-15 17:56:19 +0200

    snow Label

--- aws-tirol/main.js
+++ aws-tirol/main.js
@@ -1,6 +1,5 @@
 let basemapGray = L.tileLayer.provider('BasemapAT.grau');
 
-
 let map = L.map("map", {
     center: [47, 11],
     zoom: 9,
@@ -12,51 +11,69 @@ let map = L.map("map", {
 let layerControl = L.control.layers({
     "BasemapAT.grau": basemapGray,
     "BasemapAT.orthofoto": L.tileLayer.provider('BasemapAT.orthofoto'),
+    "BasemapAT.surface": L.tileLayer.provider('BasemapAT.surface'),
     "BasemapAT.overlay+ortho": L.layerGroup([
         L.tileLayer.provider('BasemapAT.orthofoto'),
         L.tileLayer.provider('BasemapAT.overlay')
-    ]),
-    "BasemapAT.terrain": L.tileLayer.provider('BasemapAT.terrain'),
-    "BasemapAT.surface": L.tileLayer.provider('BasemapAT.surface'),
-    "OpenTopoMap": L.tileLayer.provider('OpenTopoMap')
+    ])
 }).addTo(map);
+
 
 let awsUrl = 'https://wiski.tirol.gv.at/lawine/produkte/ogd.geojson';
 
-//Wetterstationen können an und ausgemacht werden
 let awsLayer = L.featureGroup();
 layerControl.addOverlay(awsLayer, "Wetterstationen Tirol");
-awsLayer.addTo(map);
+// awsLayer.addTo(map);
+let snowLayer = L.featureGroup();
+layerControl.addOverlay(snowLayer, "Schneehöhen");
+snowLayer.addTo(map);
+
 
 fetch(awsUrl)
     .then(response => response.json())
     .then(json => {
-        console.log('Daten konvertieren: ', json);
+        console.log('Daten konvertiert: ', json);
         for (station of json.features) {
-            console.log('Station: ', station);
-            let marker = L.marker(
-                [
-                    station.geometry.coordinates[1], station.geometry.coordinates[0]
-                ]);
-            
-            //Datum formatieren
+            // console.log('Station: ', station);
+            let marker = L.marker([
+                station.geometry.coordinates[1],
+                station.geometry.coordinates[0]
+            ]);
             let formattedDate = new Date(station.properties.date);
-
             marker.bindPopup(`
             <h3>${station.properties.name}</h3>
-                <ul>
-                    <li>Datum: ${formattedDate.toLocaleString("de")}</li>
-                    <li>Temperatur: ${station.properties.LT} °C</li>
-                    <li>Schneehöhe: ${station.properties.HS} m</li>
-                    <li>Schneehöhe: ${station.properties.RH} %</li>
-                    <li>Höhe über NHN: ${station.geometry.coordinates[2]} m</li>
-
-
-                </ul>
+            <ul>
+              <li>Datum: ${formattedDate.toLocaleString("de")}</li>
+              <li>Seehöhe: ${station.geometry.coordinates[2]} m</li>
+              <li>Temperatur: ${station.properties.LT} C</li>
+              <li>Schneehöhe: ${station.properties.HS || '?'} cm</li>
+              <li>Windgeschwindigkeit: ${station.properties.WG || '?'} km/h</li>
+              <li>Windgeschwindrichtung: ${station.properties.WR || '?'}</li>
+            </ul>
+            <a target="_blank" href="https://wiski.tirol.gv.at/lawine/grafiken/1100/standard/tag/${station.properties.plot}.png">Grafik</a>
             `);
             marker.addTo(awsLayer);
+            if (station.properties.HS) {
+                let highlightClass = '';
+                if (station.properties.HS > 100) {
+                    highlightClass = 'snow-100';
+                }
+                if (station.properties.HS > 200) {
+                    highlightClass = 'snow-200';
+                }
+                let snowIcon = L.divIcon({
+                    html: `<div class="snow-label ${highlightClass}">${station.properties.HS}</div>`
+                })
+                let snowMarker = L.marker([
+                    station.geometry.coordinates[1],
+                    station.geometry.coordinates[0]
+                ], {
+                    icon: snowIcon
+                });
+                snowMarker.addTo(snowLayer);
+            }
         }
-
-        //set map view to all stations
+        
+        // set map view to all stations
         map.fitBounds(awsLayer.getBounds());
     });
