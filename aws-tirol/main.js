35c4103a9cc473300b179b754afa08e8d2102598
geo6371 <jens-weise@hotmail.de>
2021-04-15 17:12:17 +0200

    fit map bounds

--- aws-tirol/main.js
+++ aws-tirol/main.js
@@ -40,4 +40,7 @@ fetch(awsUrl)
             marker.bindPopup(`<h3>${station.properties.name}</h3>`)
             marker.addTo(awsLayer);
         }
+
+        //set map view to all stations
+        map.fitBounds(awsLayer.getBounds());
     });
