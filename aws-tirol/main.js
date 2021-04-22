f8946e9df264b4d48e5bfc3f80e918e360c27a82
geo6371 <jens-weise@hotmail.de>
2021-04-22 18:29:16 +0200

    hover over Fenster und beautify File

--- aws-tirol/main.js
+++ aws-tirol/main.js
@@ -64,7 +64,8 @@ let newLabel = (coords, options) => {
         className: "text-label"
     })
     let marker = L.marker([coords[1], coords[0]], {
-        icon: label
+        icon: label,
+        title: `${options.station} (${coords[2]}m)`
     });
     return marker;
 };
@@ -101,7 +102,8 @@ fetch(awsUrl)
             if (typeof station.properties.HS == "number") {
                 let marker = newLabel(station.geometry.coordinates, {
                     value: station.properties.HS.toFixed(0),
-                    colors: COLORS.snowheight
+                    colors: COLORS.snowheight,
+                    station: station.properties.name
                 });
                 marker.addTo(overlays.snowheight);
             }
@@ -110,7 +112,8 @@ fetch(awsUrl)
             if (typeof station.properties.WG == "number") {
                 let marker = newLabel(station.geometry.coordinates, {
                     value: station.properties.WG.toFixed(0),
-                    colors: COLORS.windspeed
+                    colors: COLORS.windspeed,
+                    station: station.properties.name
                 });
                 marker.addTo(overlays.windspeed);
             }
@@ -119,7 +122,8 @@ fetch(awsUrl)
             if (typeof station.properties.LT == "number") {
                 let marker = newLabel(station.geometry.coordinates, {
                     value: station.properties.LT.toFixed(1),
-                    colors: COLORS.temperature
+                    colors: COLORS.temperature,
+                    station: station.properties.name
                 });
                 marker.addTo(overlays.temperature);
             }
@@ -128,7 +132,9 @@ fetch(awsUrl)
             if (typeof station.properties.WR == "number") {
                 let marker = newLabel(station.geometry.coordinates, {
                     value: station.properties.WR.toFixed(0),
-                    colors: COLORS.temperature
+                    // muss noch in colors.js eingefügt werden
+                    colors: COLORS.snowheight,
+                    station: station.properties.name
                 });
                 marker.addTo(overlays.winddirection);
             }
