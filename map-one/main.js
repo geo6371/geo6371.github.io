95a0a89255462caedc3d7a630ab4577ba63fdfcc
geo6371 <jens-weise@hotmail.de>
2021-05-20 19:32:50 +0200

    Einbindung json. console.log co2 Daten

--- map-one/main.js
+++ map-one/main.js
@@ -18,5 +18,35 @@ let layerControl = L.control.layers({
     "Dark Mode": baselayers.darkMode,
     "Relief": baselayers.terrain,
 //}, {
-    //"Liniennetz Vienna Sightseeing": overlays.busLines,
+  //  "Liniennetz Vienna Sightseeing": overlays.country,
 }).addTo(map);
+
+//So komme ich an die CO2-Daten
+//console.log('CO2-Daten: ', CODATA[0].Afghanistan.data[70].cement_co2);
+
+// Länderliste abrufen
+//console.log('CO2-Daten: ', CODATA[0].country);
+
+let countries = CODATA[0].country;
+console.log('Länder: ', countries)
+
+console.log(CODATA.length);
+
+//for (let i = 0; i < CODATA[0].length; i++){
+//    console.log(array[i]);
+//  }
+
+
+const selectedCountry = "data";
+
+for (let coTwo of CODATA) {
+    if (selectedCountry == coTwo) {
+        selected = 'selected';
+    } else {
+        selected = 'no';
+    }
+console.log(selected);
+}
+
+//Länder-Polygone hinzugefuegt
+L.geoJson(COUNTRY).addTo(map);
