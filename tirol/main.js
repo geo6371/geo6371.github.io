ff512524e7950f4b1f0c269d952493da7674d45d
geo6371 <jens-weise@hotmail.de>
2021-05-20 17:29:35 +0200

    drawTrack on select

--- tirol/main.js
+++ tirol/main.js
@@ -93,3 +93,8 @@ for (let track of BIKETIROL) {
     }
     pulldown.innerHTML += `<option ${selected} value="${track.nr}">${track.nr}: ${track.etappe}</option>`;
 }
+
+pulldown.onchange = () => {
+    console.log('changed!', pulldown.value)
+    drawTrack(pulldown.value);
+};
