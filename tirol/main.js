c660e890e68463b12d3cc50d10910d776f4eb3e9
geo6371 <jens-weise@hotmail.de>
2021-05-27 16:27:36 +0200

    Icon nach Typ anzeigen

--- tirol/main.js
+++ tirol/main.js
@@ -57,7 +57,18 @@ const drawWikipedia = (bounds) => {
 
         //Artikel Marker erzeugen
         for (let article of jsonData.geonames) {
-            let mrk = L.marker([article.lat, article.lng]);
+            // welches icon soll verwendet werden?
+            if (icons[article.feature]) {
+                //ein bekanntes
+            } else {
+                article.feature = "default";
+            }
+
+            let mrk = L.marker([article.lat, article.lng], {
+                icon: L.icon({
+                    iconUrl: `icons/${icons[article.feature]}`
+                })
+            });
             mrk.addTo(overlays.wikipedia);
 
             //Bild definieren
