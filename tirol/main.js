5f8e952f1b7695928eefb70265501bbc85ec6213
geo6371 <jens-weise@hotmail.de>
2021-05-27 16:16:15 +0200

    PopUp vervollstaendigt

--- tirol/main.js
+++ tirol/main.js
@@ -45,11 +45,20 @@ const drawWikipedia = (bounds) => {
             let mrk = L.marker([article.lat, article.lng]);
             mrk.addTo(overlays.wikipedia);
 
-            //PopUp erzeugen
+            //Bild definieren
             let img = "";
             if (article.thumbnailImg) {
                 img = `<img src="${article.thumbnailImg}" alt="thumbnail">`;
             }
+
+            //PopUp definieren
+            mrk.bindPopup(`
+                <small>${article.feature}</small>
+                <h3>${article.title} (${article.elevation}m)</h3>
+                ${img}
+                <p>${article.summary}</p>
+                <a target="Wikipedia" href="https://${article.wikipediaUrl}">Wikipedia-Artikel</a>
+            `)
         }
     }); 
 };
