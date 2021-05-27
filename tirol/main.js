ccb62b5e891516f00037779abae439b2db14dda5
geo6371 <jens-weise@hotmail.de>
2021-05-27 16:04:56 +0200

    Wiki-Marker anzeigen

--- tirol/main.js
+++ tirol/main.js
@@ -39,6 +39,12 @@ const drawWikipedia = (bounds) => {
         response => response.json()
     ).then(jsonData => {
         console.log(jsonData)
+
+    //Artikel Marker erzeugen
+        for (let article of jsonData.geonames) {
+            let mrk = L.marker([article.lat, article.lng]);
+            mrk.addTo(overlays.wikipedia)
+        }
     }); 
 };
 
