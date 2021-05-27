5f675c185222d838cf94b12aae062d3882dedf0d
geo6371 <jens-weise@hotmail.de>
2021-05-27 17:48:19 +0200

    Weblink verschoenert und Streckeninfos hinzu

--- tirol/main.js
+++ tirol/main.js
@@ -161,17 +161,18 @@ const selectedTrack = 25;
 drawTrack(selectedTrack);
 
 const updateTexts = (nr) => {
-    console.log(nr);
+    //console.log(nr);
     for (let etappe of BIKETIROL) {
         //console.log(etappe);
         //ist es die aktuelle Etappe?
         if (etappe.nr == nr) {
-            console.log("unsere Etappe", etappe);
+            etappe.homepage = `<a href="${etappe.weblink}">Homepage</a>`
+            //console.log("unsere Etappe", etappe);
             for (let key in etappe) {
-                console.log("key:", key, "value:", etappe[key]);
+                //console.log("key:", key, "value:", etappe[key]);
                 //gibt es ein Element in HTML mit der id von "key"?
                 if (document.querySelector(`#text-${key}`)) {
-                    console.log("YAY!", key, etappe[key]);
+                    //console.log("YAY!", key, etappe[key]);
                     document.querySelector(`#text-${key}`).innerHTML = etappe[key];
                 }
             }
